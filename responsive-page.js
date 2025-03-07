6a2030948534f819ab5053b19cd7ad92672943fa
Vitor Rodrigues Gomes <vitor_jackson1992@hotmail.com>
2025-03-07 11:49:51 -0300

    Update responsive-page.js

--- responsive-page.js
+++ responsive-page.js
@@ -1,7 +1,7 @@
 // mensagem de alerta no slide 4 (orkut) 
 function verificarTela(event, url) {
   if (window.innerWidth < 1024) {
-    const confirmar = confirm("ATENÇÃO: A visualização deste site é ideal em telas maiores. Deseja continuar?");
+    const confirmar = confirm("ATENÇÃO: A visualização deste site é ideal SOMENTE em TELAS MAIORES, pois se tratar de uma cópia fíel do Orkut. Deseja continuar ?");
     if (!confirmar) {
       event.preventDefault(); // Impede a abertura do link se o usuário cancelar
       return false;
